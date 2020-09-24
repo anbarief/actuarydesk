90b63d6a4abf4f9871d193dfab4ee6828be4f834
Arief Anbiya <35247692+anbarief@users.noreply.github.com>
2020-09-24 11:28:54 +0700

    Update __init__.py

--- actuarydesk/math_economics_viz/__init__.py
+++ actuarydesk/math_economics_viz/__init__.py
@@ -1,1 +1,7 @@
-# Version 0.2
+#version 0.2.0
+
+samples = ['federal_interest_rates_kaggle', \
+           'financial_indicators_US_stocks_kaggle', \
+           'world_development_indicator_kaggle', \
+           'average_interest_rates_US_treasury', \
+           'stochastic_random_walk']
